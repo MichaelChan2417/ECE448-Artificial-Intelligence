bff9020f58e9d02c1c639fbfa1fcf70e792bdd55
MichealChan <2417577316@qq.com>
2022-03-03 02:33:23 +0800

    mp1 finished

--- MPs/MP1/search.py
+++ MPs/MP1/search.py
@@ -18,6 +18,7 @@ import queue
 import heapq
 import queue as q
 
+
 # Search should return the path and the number of states explored.
 # The path should be a list of tuples in the form (row, col) that correspond
 # to the positions of the path taken by your search algorithm.
@@ -31,6 +32,7 @@ def search(maze, searchMethod):
         "dfs": dfs,
         "greedy": greedy,
         "astar": astar,
+        "astar_multi": astar_multi,
     }.get(searchMethod)(maze)
 
 
@@ -98,6 +100,7 @@ def dfs(maze):
     End = maze.getObjectives()[0]  # we just need the first one to get
     tempnode = Start
     flag = 0
+
     def dfs_handler(tempnode):
         nonlocal visit
         nonlocal path
@@ -117,7 +120,7 @@ def dfs(maze):
                 if visit[pos_node[0]][pos_node[1]] == 0:
                     if flag == 0:
                         dfs_handler(pos_node)
-            if flag==0:
+            if flag == 0:
                 visit[tempnode[0]][tempnode[1]] = 0
                 path.pop()
 
@@ -130,8 +133,9 @@ def dfs(maze):
 
 # The support function used in greedy's heapq for the priority decision of distance
 def distGet(End, curnode):
-    dist = abs(End[0]-curnode[0]) + abs(End[1]-curnode[1])
+    dist = abs(End[0] - curnode[0]) + abs(End[1] - curnode[1])
     return dist
+
 
 # The function to find the closest object in maze to the node.
 def manhattan_min(objectives, node):
@@ -140,9 +144,10 @@ def manhattan_min(objectives, node):
         new_dist = distGet(i, node)
         if minn > new_dist:
             minn = new_dist
-            
+
     return minn
-        
+
+
 def greedy(maze):
     # return path, num_states_explored; output init
     num_states = 0
@@ -161,7 +166,7 @@ def greedy(maze):
 
     father[Start[0]][Start[1]] = Start  # set start's self to be its father
     prio_q = []
-    heapq.heappush(prio_q, (distGet(End,Start), Start))
+    heapq.heappush(prio_q, (distGet(End, Start), Start))
 
     while len(prio_q) != 0:
         ele = heapq.heappop(prio_q)
@@ -180,7 +185,7 @@ def greedy(maze):
             visit[pos_nodes[0]][pos_nodes[1]] = 1
             # set its father to tempnode
             father[pos_nodes[0]][pos_nodes[1]] = tempnode
-            heapq.heappush(prio_q, (distGet(End,pos_nodes), pos_nodes))
+            heapq.heappush(prio_q, (distGet(End, pos_nodes), pos_nodes))
 
     # now the tempnode reach to End; we need back trace and push it into a stack for
     # path print; so count the num_states
@@ -195,12 +200,9 @@ def greedy(maze):
     return path, num_states
 
 
-    
 def astar(maze):
-    # TODO: Write your code here
-    # return path, num_states_explored
-    num_states=0
-    path=[]
+    num_states = 0
+    path = []
     # receive the parameters from maze
     Rows = maze.getDimensions()[0]
     Columns = maze.getDimensions()[1]
@@ -208,27 +210,27 @@ def astar(maze):
     visit = [[0 for i in range(Columns)] for j in range(Rows)]
     # use to record the back trace
     father = [[0 for i in range(Columns)] for j in range(Rows)]
-    path_len= [[0 for i in range(Columns)] for j in range(Rows)]
+    path_len = [[0 for i in range(Columns)] for j in range(Rows)]
     Start = maze.getStart()  # the coordinate to start
     objectives = maze.getObjectives()  # find all objectives
-    
-    prio_q=q.PriorityQueue()
-    
+
+    prio_q = q.PriorityQueue()
+
     father[Start[0]][Start[1]] = Start  # set start's self to be its father
     # print(manhattan_min(objectives,Start))
-    prio_q.put((manhattan_min(objectives,Start),Start))
-    
+    prio_q.put((manhattan_min(objectives, Start), Start))
+
     while prio_q:
         ele = prio_q.get()
-        
+
         tempnode = ele[1]
-        
+
         # print("tempnode: ")
         # print(tempnode)
         if tempnode in objectives:
             break
         # it's not end; then set it visited and find its valid neighbor
-        
+
         visit[tempnode[0]][tempnode[1]] = 1
         valid_neighbors = maze.getNeighbors(tempnode[0], tempnode[1])
         for pos_nodes in valid_neighbors:
@@ -240,7 +242,7 @@ def astar(maze):
             # set its father to tempnode
             path_len[pos_nodes[0]][pos_nodes[1]] = path_len[tempnode[0]][tempnode[1]] + 1
             father[pos_nodes[0]][pos_nodes[1]] = tempnode
-            prio_q.put((manhattan_min(objectives, pos_nodes)+path_len[pos_nodes[0]][pos_nodes[1]], pos_nodes))
+            prio_q.put((manhattan_min(objectives, pos_nodes) + path_len[pos_nodes[0]][pos_nodes[1]], pos_nodes))
     # print("here")
     # now the tempnode reach to End; we need back trace and push it into a stack for
     # path print; so count the num_states
@@ -253,7 +255,142 @@ def astar(maze):
     path.reverse()
     return path, num_states
 
-# def astr_multi(maze):
+
+# the function to create mst over the objectives;
+# root is the root node(tuple); Objectives is the list of tuples with objects
+def mst(index, Objectives):
+    n = len(Objectives)
+    not_in_list = [i for i in range(n)]
+    connected_Matrix = [[0 for i in range(n)] for j in range(n)]
+    dist_Matrix = [[0 for i in range(n)] for j in range(n)]
+    # construct the dist_Matrix first
+    for i in range(n):
+        for j in range(i+1):
+            dist_Matrix[i][j] = distGet(Objectives[i], Objectives[j])
+            dist_Matrix[j][i] = distGet(Objectives[j], Objectives[i])
+        connected_Matrix[i][i] = 1
+
+    contained = [index]
+    not_in_list.remove(index)
+
+    while len(not_in_list) > 0:
+        min_idx = not_in_list[0]
+        min_cor_idx = contained[0]
+        min_dist = dist_Matrix[min_idx][min_cor_idx]
+        # for each idx we make the compare
+        for idx in not_in_list:
+            for orgidx in contained:
+                if dist_Matrix[idx][orgidx] < min_dist:
+                    min_idx = idx
+                    min_cor_idx = orgidx
+                    min_dist = dist_Matrix[idx][orgidx]
+        connected_Matrix[min_idx][min_cor_idx] = 1
+        connected_Matrix[min_cor_idx][min_idx] = 1
+        contained.append(min_idx)
+        not_in_list.remove(min_idx)
 
 
-    
+    return connected_Matrix
+
+
+def astar_multi(maze):
+    tpath = []
+    num_states = 0
+    # receive the parameters from maze
+    Rows = maze.getDimensions()[0]
+    Columns = maze.getDimensions()[1]
+
+    # Objectives is the list of tuples with objects
+    Objectives = maze.getObjectives()
+
+    # then we find the closest objects and start inserting path
+    Start_Point = maze.getStart()
+    index = 0
+    min_dist = distGet(Start_Point, Objectives[0])
+    for i in range(len(Objectives)):
+        if distGet(Start_Point, Objectives[i]) < min_dist:
+            index = i
+            min_dist = distGet(Start_Point, Objectives[i])
+
+    Matrix = mst(index, Objectives)
+
+    # now index holds the closest index to Start Point
+    # We have two parts in finding the path
+    # 1. find the path from Start Point to Objectives[index]
+    # 2. traverse in the Objectives' MST to complete the path
+
+    # support function in astar_multi to find the minimum path between SP and EP, added to the path
+    def Path_Add(SP, EP):
+        path = []
+        num_states = 0
+        # use to store whether this point have been visited
+        visit = [[0 for i in range(Columns)] for j in range(Rows)]
+        # use to record the back trace
+        father = [[0 for i in range(Columns)] for j in range(Rows)]
+        path_len = [[0 for i in range(Columns)] for j in range(Rows)]
+
+        prio_q = q.PriorityQueue()
+        father[SP[0]][SP[1]] = SP  # set start's self to be its father
+        prio_q.put((distGet(SP, EP), SP))
+
+        while prio_q:
+            ele = prio_q.get()
+            tempnode = ele[1]
+            # now we find the final EP
+            if tempnode == EP:
+                break
+            # it's not end; then set it visited and find its valid neighbor
+            visit[tempnode[0]][tempnode[1]] = 1
+            valid_neighbors = maze.getNeighbors(tempnode[0], tempnode[1])
+
+            for pos_nodes in valid_neighbors:
+                # print(pos_nodes,path_len[tempnode[0]][tempnode[1]]+1)
+                if visit[pos_nodes[0]][pos_nodes[1]]:
+                    continue
+                # now it has been visited
+                visit[pos_nodes[0]][pos_nodes[1]] = 1
+                # set its father to tempnode
+                path_len[pos_nodes[0]][pos_nodes[1]] = path_len[tempnode[0]][tempnode[1]] + 1
+                father[pos_nodes[0]][pos_nodes[1]] = tempnode
+                prio_q.put((distGet(pos_nodes, EP) + path_len[pos_nodes[0]][pos_nodes[1]], pos_nodes))
+        # now start print node to path
+        while tempnode != SP:
+            path.append(tempnode)
+            next_node = father[tempnode[0]][tempnode[1]]
+            num_states += 1
+            tempnode = next_node
+        path.append(SP)
+        path.reverse()
+        return path, num_states
+
+    # first add the path from start to nearest obj
+    temp_path, temp_num_states = Path_Add(Start_Point, Objectives[index])
+    tpath = tpath + temp_path
+    num_states += temp_num_states
+
+    # then is the recursive part to traverse the tree
+    def RecurTree(Matrix, cur_idx, last_idx):
+        nonlocal tpath
+        nonlocal num_states
+        # find the possible next_node first
+        pos_next_set = []
+        for i in range(len(Matrix)):
+            if Matrix[cur_idx][i] == 1 and i != cur_idx and i != last_idx:
+                pos_next_set.append(i)
+        # then find the path to these nodes
+        for pos_index in pos_next_set:
+            pathi, i_numstates = Path_Add(Objectives[cur_idx], Objectives[pos_index])
+            tpath = tpath + pathi
+            num_states += i_numstates
+
+            # then recursive in the new node
+            RecurTree(Matrix, pos_index, cur_idx)
+
+            # when get back, add the reversed path
+            pathi.reverse()
+            tpath = tpath + pathi
+            num_states += i_numstates
+
+    RecurTree(Matrix, index, len(Objectives) + 1)
+
+    return tpath, num_states
